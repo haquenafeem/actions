60edcacadb045852f5e7783d7c797cc3ae060937
Nafeem Haque <haquenafeem@gmail.com>
2021-04-03 12:35:46 +0600

    updates

--- cmd/main.go
+++ cmd/main.go
@@ -7,5 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println(calculator.Add(3, 4))
+	fmt.Println("3 + 4 =", calculator.Add(3, 4))
+	fmt.Println("25 - 6 =", calculator.Subtract(25, 6))
+	fmt.Println("15 * 6 =", calculator.Multiply(15, 6))
 }
