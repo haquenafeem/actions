55905e21b76bdcf036404024da22d3b8519c8ed7
Nafeem Haque <haquenafeem@gmail.com>
2021-04-03 13:58:44 +0600

    panic removed

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/haquenafeem/actions/calculator"
@@ -11,6 +10,4 @@ func main() {
 	fmt.Println("3 + 4 =", calculator.Add(3, 4))
 	fmt.Println("25 - 6 =", calculator.Subtract(25, 6))
 	fmt.Println("15 * 6 =", calculator.Multiply(15, 6))
-
-	panic(errors.New("test panic"))
 }
