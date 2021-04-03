e5b96e0903c7e8ce8b241e1936c877a21024a6e6
Nafeem Haque <haquenafeem@gmail.com>
2021-04-03 12:30:49 +0600

    added multiply

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -4,6 +4,7 @@ import "testing"
 
 var AddResultSet = []struct{ Input1, Input2, Result int }{{1, 2, 3}, {4, 5, 9}}
 var SubtractResultSet = []struct{ Input1, Input2, Result int }{{3, 2, 1}, {6, 3, 3}}
+var MultiplyResultSet = []struct{ Input1, Input2, Result int }{{3, 2, 6}, {6, 3, 18}}
 
 func TestAdd(t *testing.T) {
 	for _, v := range AddResultSet {
@@ -19,6 +20,17 @@ func TestAdd(t *testing.T) {
 func TestSubtract(t *testing.T) {
 	for _, v := range SubtractResultSet {
 		result := Subtract(v.Input1, v.Input2)
+		expected := v.Result
+
+		if result != expected {
+			t.Fail()
+		}
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	for _, v := range MultiplyResultSet {
+		result := Multiply(v.Input1, v.Input2)
 		expected := v.Result
 
 		if result != expected {
