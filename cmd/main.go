11be3d74143c504ba67197ff1dc843be0ce74221
haquenafeem <haquenafeem@gmail.com>
2021-07-03 13:50:17 +0600

    change

--- cmd/main.go
+++ cmd/main.go
@@ -10,5 +10,6 @@ func main() {
 	fmt.Println("3 + 4 =", calculator.Add(3, 4))
 	fmt.Println("25 - 6 =", calculator.Subtract(25, 6))
 	fmt.Println("15 * 6 =", calculator.Multiply(15, 6))
+	fmt.Println("15 * 4 =", calculator.Multiply(15, 4))
 	// xyz
 }
